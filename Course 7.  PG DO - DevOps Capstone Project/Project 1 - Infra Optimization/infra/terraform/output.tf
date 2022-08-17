5b16200a4e2e17594d7f5ddf4da39b1c61bb7f64
mstr0m <evaq88@gmail.com>
2022-08-17 14:25:50 +0000

    output url only

--- Course 7.  PG DO - DevOps Capstone Project/Project 1 - Infra Optimization/infra/terraform/output.tf
+++ Course 7.  PG DO - DevOps Capstone Project/Project 1 - Infra Optimization/infra/terraform/output.tf
@@ -1,7 +1,3 @@
-output "k8s_master_ip" {
-  description = "Local ~/.kube/config is ready. k8s master ip:"
-  value       = "${aws_instance.k8s-master.public_ip}"
-}
 output "app_url" {
   description = "Wordpress app url:"
   value       = "http://${aws_instance.k8s-master.public_ip}:31000"
